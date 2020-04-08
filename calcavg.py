bc70838369ecff9002c599d6b6ea919f9fc2755a
sopasolari <sopasolari@hotmail.com>
2020-04-08 05:47:33 +0300

    edit caclavg.py

--- calcavg.py
+++ calcavg.py
@@ -2,4 +2,3 @@ stdgrades = {'Marios': 15.3 , 'Peter': 9.9 , 'George': 19.9 ,'john': 3}
 
 print("The average grades of yours student is:",sum(stdgrades.values()) / len(stdgrades.values()))
 print("The max it is:",max(stdgrades.values()))
-print(stdgrades)
