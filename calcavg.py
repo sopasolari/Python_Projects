c16efd5e40b0b1bf5b5b4780c168711b5767ac9e
sopasolari <sopasolari@hotmail.com>
2020-04-13 10:07:54 +0300

    upgrade loop inside calcavg.py

--- calcavg.py
+++ calcavg.py
@@ -1,8 +1,8 @@
 averg = []
-averg.append(float(input("Enter the 1 value for avarage:")))
-averg.append(float(input("Enter the 2 value for avarage:")))
-averg.append(float(input("Enter the 3 value for avarage:")))
-averg.append(float(input("Enter the 4 value for avarage:")))
+userinp = int(input("How values you want to add:"))
+for x in range(userinp):
+    averg.append(float(input("Enter the %d value for avarage:" % (x+1))))
 print (averg)
+
 print("The average grades of yours student is:", sum(averg) / len(averg))
 print("The max it is:",max(averg))
