70ba78735754f49a71b52f674d73eb53b3679a53
sopasolari <sopasolari@hotmail.com>
2020-04-18 05:56:48 +0300

    modif practice.py

--- practice.py
+++ practice.py
@@ -1,6 +1,19 @@
-x=1
-y=2
-if x == 1 and y == 2:
-    print ("yes")
-else:
-    print("no")
+def cap(check):
+    ins = ('how', 'what', 'why')
+    capitalized = check.capitalize()
+    if check.startswith(ins):
+        return "{}?".format(capitalized)
+    else:
+        return "{}.".format(capitalized)
+check = ""
+str = ""
+while True:
+    check = input("Say something:")
+    if check != "/end":
+        str += cap(check)
+        str += " "
+        print (str)
+        continue
+    else:
+        print (str)
+        break
