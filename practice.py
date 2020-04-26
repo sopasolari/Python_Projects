df251b5258eaafae1f0b7b5fb7daddcdcad38415
sopasolari <sopasolari@hotmail.com>
2020-04-26 19:12:00 +0300

    update original.txt practice.py

--- practice.py
+++ practice.py
@@ -1,19 +1,2 @@
-def cap(check):
-    ins = ('how', 'what', 'why')
-    capitalized = check.capitalize()
-    if check.startswith(ins):
-        return "{}?".format(capitalized)
-    else:
-        return "{}.".format(capitalized)
-check = ""
-str = ""
-while True:
-    check = input("Say something:")
-    if check != "/end":
-        str += cap(check)
-        str += " "
-        print (str)
-        continue
-    else:
-        print (str)
-        break
+file = open("original.txt")
+print(file.read())
