2d98b4a95e490d1d1ce77d9a4187580e93e425ed
sopasolari <sopasolari@hotmail.com>
2020-05-01 17:21:40 +0300

    update dectionery.py

--- Dictionery/dictionery.py
+++ Dictionery/dictionery.py
@@ -4,9 +4,10 @@ with open("data.json") as reader:
     data = json.load(reader)
 
 def  Word_Meaning(user_input):
+    user_input = user_input.lower()
     if user_input in data :
         return data[user_input]
     else :
-        return "The word not found. Please try again!"
+        return "The word not found. Please try another word!"
 
 print(Word_Meaning(input("Enter a word: ")))
