79bcee0e538cf21650364b6e64c44185d769c838
sopasolari <sopasolari@hotmail.com>
2020-05-01 03:37:48 +0300

    fix if word not exist on dictionery

--- Dictionery/dictionery.py
+++ Dictionery/dictionery.py
@@ -4,6 +4,9 @@ with open("data.json") as reader:
     data = json.load(reader)
 
 def  Word_Meaning(user_input):
-    return data[user_input]
+    if user_input in data :
+        return data[user_input]
+    else :
+        return "The word not found. Please try again!"
 
 print(Word_Meaning(input("Enter a word: ")))
