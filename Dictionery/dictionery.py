94d7678a41e138ca5b6396c2779d62f2b0149379
sopasolari <sopasolari@hotmail.com>
2020-05-05 02:30:44 +0300

    update comments and improve codes for Dictionery V1.0

--- Dictionery/dictionery.py
+++ Dictionery/dictionery.py
@@ -1,27 +1,29 @@
-import json
-import difflib
-from difflib import SequenceMatcher,get_close_matches
+import json #Import json and get_close_matches from difflib header
+from difflib import get_close_matches 
 
-with open("data.json") as reader:
-    data = json.load(reader)
+with open("data.json") as reader: #Open file data.json
+    data = json.load(reader) #we store all the json file to variable data
 
-def  Word_Meaning(user_input):
-    user_input = user_input.lower()
+def  Word_Meaning(user_input): #Function to return the meaning of the word
+    user_input = user_input.lower()#Make the user input word lowercase
     
-    if user_input in data :
-        return data[user_input]
+    if user_input in data : # Check if the user input value it is in the dictionery
+        return data[user_input] #return the meaning of the word
+    elif user_input.capitalize() in data :#Check for the capitals for example Paris
+        return data[user_input.capitalize()]
     else:
-        possibility = get_close_matches(user_input,data.keys(),cutoff=0.8)
-        if not possibility:
-            return "The word not found. Please try another word!"
+        possibility = get_close_matches(user_input,data.keys(),cutoff=0.8) #Assign in variable the possibility word
+        if not possibility: #Check if the dict is empty!
+            return {"The word not found. Please try another word!"}
         else :
-            possibility = possibility[0]
-            print(SequenceMatcher(None,user_input,possibility).ratio())
-            user_ans = input("Did you mean " + possibility + "? pres y for yes or n for no:")
-            user_ans = user_ans.lower()
+            possibility = possibility[0] #Renew the value possibility and we take the first word from dictonery
+            user_ans = input("Did you mean " + possibility + " instead? Enter y for yes or n for no:") #Ask the user if is correct word
+            user_ans = user_ans.lower() #Make the answer lower to check with y or n
             if user_ans == "y":
                 return data[possibility]
+            elif user_ans == "n":
+                return {"The word not found. Please double check!"} #If user dont press n/N we return this message
             else :
-                return "The word not found. Please try another word!"
+                return {"We don't understand your choice!"} #If user dont press y/Y or n/N we return this message
 
-print(Word_Meaning(input("Enter a word: ")))
+print(*Word_Meaning(input("Enter a word: ")),sep='\n') #Ask the user to enter a word and call the function Word_Meaning
