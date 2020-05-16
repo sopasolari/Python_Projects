d3bf561beb66550c7068cbc18c85a02254d051a5
sopasolari <sopasolari@hotmail.com>
2020-05-17 02:06:46 +0300

    Add the first test marker

--- Web maps/Map1.py
+++ Web maps/Map1.py
@@ -1,3 +1,4 @@
 import folium
-map = folium.Map(location=[50,50], zoom_start=15, tiles="Stamen Terrain")
+map = folium.Map(location=[37.976445, 23.725094], zoom_start=6, tiles="Stamen Terrain")
+map.add_child(folium.Marker(location=[37.9,23.7],popup="Hello from Greece!",icon=folium.Icon(color="orange")))
 map.save("Map1.html")
