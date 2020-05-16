fd463ee82b7460830121b1a33145a762c14ce18c
sopasolari <sopasolari@hotmail.com>
2020-05-17 02:15:13 +0300

    create a feature group

--- Web maps/Map1.py
+++ Web maps/Map1.py
@@ -1,4 +1,8 @@
 import folium
+
 map = folium.Map(location=[37.976445, 23.725094], zoom_start=6, tiles="Stamen Terrain")
-map.add_child(folium.Marker(location=[37.9,23.7],popup="Hello from Greece!",icon=folium.Icon(color="orange")))
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[38,24],popup="Hello from Greece!",icon=folium.Icon(color="orange")))
+map.add_child(fg)
+
 map.save("Map1.html")
