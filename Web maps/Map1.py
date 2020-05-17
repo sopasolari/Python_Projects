bc5519f8323887e9227da0bd5e04b44126358750
sopasolari <sopasolari@hotmail.com>
2020-05-17 15:06:31 +0300

    make circle dynamic marker's

--- Web maps/Map1.py
+++ Web maps/Map1.py
@@ -21,7 +21,13 @@ name = list(data["NAME"])
 
 
 for lt,ln, el in zip(lat,lon,elev):
-    fg.add_child(folium.Marker(location=[lt,ln],popup=str(el) + " m",icon=folium.Icon(color=color_select(el))))
+    fg.add_child(folium.CircleMarker(location=[lt,ln],
+    radius=10,
+    popup=str(el) + " m",
+    fill_color=color_select(el),
+    fill=True,
+    color="gray",
+    fill_opacity=0.7))
 map.add_child(fg)
 
 map.save("Map1.html")
