375b38387d6b9bc18696f0e85f89b7c916285620
sopasolari <sopasolari@hotmail.com>
2020-05-17 03:11:45 +0300

    add volcanoes marked at maps

--- Web maps/Map1.py
+++ Web maps/Map1.py
@@ -1,8 +1,16 @@
 import folium
+import pandas
 
-map = folium.Map(location=[37.976445, 23.725094], zoom_start=6, tiles="Stamen Terrain")
+map = folium.Map(location=[35.5, -110], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38,24],popup="Hello from Greece!",icon=folium.Icon(color="orange")))
+
+data = pandas.read_csv("Volcanoes.txt")
+lon = list(data["LON"])
+lat = list(data["LAT"])
+elev = list(data["ELEV"])
+
+for lt,ln, el in zip(lat,lon,elev):
+    fg.add_child(folium.Marker(location=[lt,ln],popup=el,icon=folium.Icon(color="green")))
 map.add_child(fg)
 
 map.save("Map1.html")
