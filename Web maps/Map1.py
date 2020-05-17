f76b34279e08b65ffb9b348960abfd0a7a75ce58
sopasolari <sopasolari@hotmail.com>
2020-05-17 14:46:30 +0300

    make the markers dynamic

--- Web maps/Map1.py
+++ Web maps/Map1.py
@@ -1,6 +1,15 @@
 import folium
 import pandas
 
+def color_select(eleve):
+    if eleve < 1000 :
+        return "green"
+    elif eleve >= 1000 and eleve < 3000 :
+        return "orange"
+    else :
+        return "red"
+
+
 map = folium.Map(location=[35.5, -110], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
@@ -8,9 +17,11 @@ data = pandas.read_csv("Volcanoes.txt")
 lon = list(data["LON"])
 lat = list(data["LAT"])
 elev = list(data["ELEV"])
+name = list(data["NAME"])
+
 
 for lt,ln, el in zip(lat,lon,elev):
-    fg.add_child(folium.Marker(location=[lt,ln],popup=el,icon=folium.Icon(color="green")))
+    fg.add_child(folium.Marker(location=[lt,ln],popup=str(el) + " m",icon=folium.Icon(color=color_select(el))))
 map.add_child(fg)
 
 map.save("Map1.html")
