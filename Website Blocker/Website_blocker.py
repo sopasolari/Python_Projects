f96261cf6d4aadb64490cc3b31b60fba861050b9
Fastman9999 <sopasolari@hotmail.com>
2020-05-19 01:15:33 +0300

    add paths in the file

---
+++ Website Blocker/Website_blocker.py
@@ -0,0 +1,8 @@
+import time
+
+hosts_path=r"C:\Windows\System32\drivers\etc"
+redirect="127.0.0.1"
+website_list=["facebook.com","www.facebook.com","pronews.gr","www.pronews.gr"]
+
+while True:
+    print(1)
