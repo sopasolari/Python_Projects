9513acdffa8ba7dba7c59ad03cdc386c28f33dbf
Fastman9999 <sopasolari@hotmail.com>
2020-05-19 01:48:56 +0300

    add checking the time

--- Website Blocker/Website_blocker.py
+++ Website Blocker/Website_blocker.py
@@ -1,8 +1,14 @@
 import time
+from datetime import datetime as dt
 
 hosts_path=r"C:\Windows\System32\drivers\etc"
 redirect="127.0.0.1"
 website_list=["facebook.com","www.facebook.com","pronews.gr","www.pronews.gr"]
 
 while True:
-    print(1)
+    if dt(dt.now().year,dt.now().month,dt.now().day,10) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,18):
+        print("Working hours...")
+    else :
+        print("Relaxing Time...")
+
+    time.sleep(5)
