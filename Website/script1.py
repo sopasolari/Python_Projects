c1ca6ee9ccc1a4bcc878828a616afb3c06a234f0
Fastman9999 <sopasolari@hotmail.com>
2020-05-25 17:40:49 +0300

    add links home and about and layout.html

--- Website/script1.py
+++ Website/script1.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route('/About/')
+@app.route('/about/')
 
 def about():
-    return "Welcome to About!"
+    return render_template("about.html")
 
 if __name__ == "__main__" :
     app.run(debug=True)
