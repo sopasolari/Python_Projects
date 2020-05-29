2f2e36aa31a4f5413d41ef5d2942055b6654de9c
Fastman9999 <sopasolari@hotmail.com>
2020-05-29 20:44:19 +0300

    add 3 text and start convert function

--- practice project/multiwidget.py
+++ practice project/multiwidget.py
@@ -2,6 +2,9 @@ from tkinter import *
 
 window=Tk()
 
+def convert():
+    t1.insert(END,float(e1_value.get())*1000)
+
 l1=Label(window,text="Kg",height=1,width=20)
 l1.grid(row=0,column=0)
 
@@ -10,7 +13,16 @@ e1_value=StringVar()
 e1 = Entry(window,textvariable=e1_value)
 e1.grid(row=0,column=1)
 
-b1=Button(window,text="Convert",height=1,width=20)
+b1=Button(window,text="Convert",height=1,width=10,command=convert)
 b1.grid(row=0,column=2)
+
+t1=Text(window,height=1,width=20)
+t1.grid(row=1,column=0)
+
+t2=Text(window,height=1,width=20)
+t2.grid(row=1,column=1)
+
+t3=Text(window,height=1,width=20)
+t3.grid(row=1,column=2)
 
 window.mainloop()
