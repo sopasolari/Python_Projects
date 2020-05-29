1814b2662d46d53cb4c3eadb962a43b47c6d88a6
Fastman9999 <sopasolari@hotmail.com>
2020-05-29 16:54:34 +0300

    remove 2 non importad files

--- practice.py
+++
@@ -1,7 +0,0 @@
-def romanToInt(inp):
-    dicton = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-    sum = 0
-    for i in range(len(inp)):
-        sum = sum + dicton[i]
-    return sum
-print(romanToInt('VII'))
