99c3c79cfc1332e5493aa6a386ebf371b7f4911e
Fastman9999 <sopasolari@hotmail.com>
2020-05-29 18:28:41 +0300

    add function km to miles

--- practice project/script1.py
+++ practice project/script1.py
@@ -2,10 +2,16 @@ from tkinter import *
 
 window=Tk()
 
-b1=Button(window,text="Excute")
+def km_to_miles():
+    print(e1_value.get())
+    t1.insert(END,float(e1_value.get())*1.6)
+
+b1=Button(window,text="Excute",command=km_to_miles)
 b1.grid(row=0,column=0)
 
-e1=Entry(window)
+e1_value=StringVar()
+
+e1=Entry(window,textvariable=e1_value)
 e1.grid(row=0,column=1)
 
 t1=Text(window,height=1,width=20)
